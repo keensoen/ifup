ce060d805aef778fd0816761749172a0837c68e9
KeennessSolutions <sesughagbadu@yahoo.com>
2020-07-07 12:12:26 +0100

    logo adjustment

--- resources/views/header.blade.php
+++ resources/views/header.blade.php
@@ -1,5 +1,5 @@
 <div class="width-mobile-auto m-0 align-items-center justify-content-center p-0 bg-transparent bg-img-none shadow-0 height-9">
     <a href="javascript:void(0)" class="page-logo-link press-scale-down d-flex align-items-center" style="margin-top:10px;">
-        <img src="{{ URL::to('img/logo.png') }}"  width="50" height="50" alt="iFollowUp" aria-roledescription="logo">
+        <img src="{{ URL::to('img/logo.png') }}" width="50" height="50" alt="iFollowUp" aria-roledescription="logo">
     </a>
 </div>
--- resources/views/layouts/partials/header.blade.php
+++ resources/views/layouts/partials/header.blade.php
@@ -2,7 +2,7 @@
     <!-- we need this logo when user switches to nav-function-top -->
     <div class="page-logo">
         <a href="javascript:void(0);" class="page-logo-link press-scale-down d-flex align-items-center position-relative">
-            <img src="{{ URL::to('img/logo.png') }}" width="50" height="50" alt="iFollowUP" aria-roledescription="logo">
+            <img src="{{ URL::to('img/logo.png') }}" style="width:100px;height:50px;" alt="iFollowUP" aria-roledescription="logo">
             <span class="position-absolute text-white opacity-50 small pos-top pos-right mr-2 mt-n2"></span>
             <i class="fal fa-angle-down d-inline-block ml-1 fs-lg color-primary-300"></i>
         </a>
