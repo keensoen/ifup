5eddaa8911fe56543206ee09ddb1beb6e0d80d29
Keenness Solutions <sesughagbadu@yahoo.com>
2020-07-11 11:17:57 +0000

    remove telescope

--- config/app.php
+++ config/app.php
@@ -174,7 +174,6 @@ return [
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
-        App\Providers\TelescopeServiceProvider::class,
 
         //Addon providers
         Intervention\Image\ImageServiceProvider::class,
