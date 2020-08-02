a2f414a54ebc319fda99ee8bdedf451a333acea7
Keenness Solutions <sesughagbadu@yahoo.com>
2020-08-02 10:40:02 +0100

    map eye

--- resources/views/member/heat_map.blade.php
+++ resources/views/member/heat_map.blade.php
@@ -52,7 +52,7 @@
       var script = document.createElement('script');
       // This example uses a local copy of the GeoJSON stored at
       // http://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/2.5_week.geojsonp
-      script.src = 'http://developers.google.com/maps/documentation/javascript/examples/json/earthquake_GeoJSONP.js';
+      script.src = 'https://developers.google.com/maps/documentation/javascript/examples/json/earthquake_GeoJSONP.js';
       document.getElementsByTagName('head')[0].appendChild(script);
     }
 
